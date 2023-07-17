50cc72af36eabe9a63889cd7edaa24a800495fa9
Andrew <andrew.foo@hotmail.com>
2023-07-17 12:30:49 +1000

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,14 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-var firebaseConfig = {
-	apiKey: `${process.env.NEXT_PUBLIC_Firebase_API_Key}`,
-	authDomain: `${process.env.NEXT_PUBLIC_Auth_Domain}`,
-	projectId: `${process.env.NEXT_PUBLIC_Project_Id}`,
-	storageBucket: `${process.env.NEXT_PUBLIC_Storage_Bucket}`,
-	messagingSenderId: `${process.env.NEXT_PUBLIC_Message_Sender_Id}`,
-	appId: `${process.env.NEXT_PUBLIC_App_Id}`
-};
+const firebaseConfig = {
+	apiKey: "AIzaSyBy4viM6Ok8TfvsqUyN7E1XosuAgO76Kwo",
+	authDomain: "oz-locums.firebaseapp.com",
+	projectId: "oz-locums",
+	storageBucket: "oz-locums.appspot.com",
+	messagingSenderId: "793509347370",
+	appId: "1:793509347370:web:082ba891fe64476d580353",
+	measurementId: "G-7Q4JHRTB26"
+  };
+
 
 let fire;
 
